69a8c80609cbab37a3b5db6c01c047257a7e6017
Oleg Onishchenko <guss20031227@gmail.com>
2024-02-13 21:47:40 +0300

    added explanations

--- Server/Clients.h
+++ Server/Clients.h
@@ -19,16 +19,17 @@ public:
 	Client(int Id, std::string Nickname, std::string password) : User_id(Id), Nickname(Nickname), password(password),
 		txt_msg_sock(nullptr), file_msg_sock(nullptr), acc_data_sock(nullptr) {}
 
+	//user data getters
 	int get_UserId() { return this->User_id; }
-
 	std::string get_nickname() { return this->Nickname; }
-
 	std::string get_password() { return this->password; }
 
+	//socket setters
 	void set_txt_msg_sock(ip::tcp::socket&& socket) { txt_msg_sock = std::make_unique<ip::tcp::socket>(std::move(socket)); }
 	void set_file_msg_sock(ip::tcp::socket&& socket) { file_msg_sock = std::make_unique<ip::tcp::socket>(std::move(socket)); }
 	void set_acc_data_sock(ip::tcp::socket&& socket) { acc_data_sock = std::make_unique<ip::tcp::socket>(std::move(socket)); }
 
+	//sockets
 	std::unique_ptr<ip::tcp::socket> txt_msg_sock;
 	std::unique_ptr<ip::tcp::socket> file_msg_sock;
 	std::unique_ptr<ip::tcp::socket> acc_data_sock;
